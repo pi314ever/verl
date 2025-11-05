120264b43d5eb0978780d815747665adc171c2e5
Daniel Huang <daniel1.huang@intel.com>
2025-11-04 17:13:39 -0800

    Add tee to run script
    Signed-off-by: Daniel Huang <daniel1.huang@intel.com>

--- run_one_step_off.sh
+++ run_one_step_off.sh
@@ -1,3 +1,3 @@
 TRAIN_PATH="$HOME/verl/data/gsm8k/train.parquet"
 [[ -f "$TRAIN_PATH" ]] || python ./examples/data_preprocess/gsm8k.py --local_save_dir "$(dirname "$TRAIN_PATH")"
-RAY_DEDUP_LOGS=0 bash ./recipe/one_step_off_policy/grpo_0.6b_gsm8k_fsdp2_2_6.sh
+RAY_DEDUP_LOGS=0 bash ./recipe/one_step_off_policy/grpo_0.6b_gsm8k_fsdp2_2_6.sh | tee one_step_off.log
